f4252d830733b56d2a186a4a91d690daf38a7869
Faith <98213736+faithdanghuy@users.noreply.github.com>
2022-07-27 22:28:57 +0700

    Selection Sort

--- Sorting Algorithm/SelectionSort.cpp
+++ Sorting Algorithm/SelectionSort.cpp
@@ -10,18 +10,3 @@ void selectionSort(int a[], int left, int right){
         swap(a[i],a[min]);
     }
 }
-
-void print(int a[], int n){
-    for(int i = 0; i < n; i++)
-        cout<<i<<" ";
-    cout<<endl;
-}
-
-int main(){
-    int arr[] = {1,8,4,6,2,9,0};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    selectionSort(arr,0,n - 1);
-    print(arr,n);
-    system("pause");
-    return 0;
-}
